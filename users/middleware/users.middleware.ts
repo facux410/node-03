2475ab1aa57442d6e493006e3a2b19384e58e2e5
facu410 <facundito410@gmail.com>
2022-06-15 21:13:16 -0300

    feat : routes with middlewares

--- users/middleware/users.middleware.ts
+++ users/middleware/users.middleware.ts
@@ -73,6 +73,14 @@ class UsersMiddleware {
       });
     }
   }
+  async extractUserId(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    req.body.id = req.params.userId;
+    next();
+  }
 }
 
 export default new UsersMiddleware();
